e8af665c04444ae514c41e1b209885c5d0bef0a1
usaid4796 <140539737+usaid4796@users.noreply.github.com>
2024-09-07 21:58:13 +0500

    All Questions Added

---
+++ questions.js
@@ -0,0 +1,223 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "HyperText Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "HyperText Markup Language",
+      "Hypar Text Markup Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Cascading Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheat"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the largest heading in HTML?",
+    answer: "h1",
+    options: [
+      "h1",
+      "h3",
+      "h6",
+      "h7" 
+    ]
+  },
+  {
+    numb: 4,
+    question: "How do you insert a line break in HTML?",
+    answer: "br",
+    options: [
+      "break",
+      "lb",
+      "Hr",
+      "br" 
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which tag is used to create a hyperlink?",
+    answer: "a",
+    options: [
+      "link",
+      "href",
+      "a",
+      "url"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which property changes the background color in CSS?",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "background-color",
+      "color",
+      "background"
+    ]
+  },
+  {
+    numb: 7,
+    question: "How do you add a border in CSS?",
+    answer: "border: 1px solid black;",
+    options: [
+      "border-style",
+      "border-width",
+      "border: 1px solid black;",
+      "border-border"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How do you center an element horizontally in CSS?",
+    answer: "margin: 0 auto;",
+    options: [
+      "text-align: center;",
+      "margin: 0 auto;",
+      "position: center;",
+      "margin: center;"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which property controls text size in CSS?",
+    answer: "font-size",
+    options: [
+      "text-size",
+      "font-style",
+      "font-size",
+      "text-style"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which property changes the font of text in CSS?",
+    answer: "font-family",
+    options: [
+      "font-style",
+      "text-font",
+      "font-family",
+      "font-weight"
+    ]
+  },
+  {
+    numb: 11,
+    question: "How do you apply a CSS class to an element?",
+    answer: ".classname",
+    options: [
+      "#classname",
+      ".classname",
+      "classname",
+      "@classname"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which property changes text color in CSS?",
+    answer: "color",
+    options: [
+      "text-color",
+      "color",
+      "font-color",
+      "background-color"
+    ]
+  },
+  {
+    numb: 13,
+    question: "How do you add padding to all sides of an element?",
+    answer: "padding: 10px;",
+    options: [
+      "padding: 10px;",
+      "margin: 10px;",
+      "padding-sides: 10px;",
+      "padding-all: 10px;"
+    ]
+  },
+  {
+    numb: 14,
+    question: "How do you make text bold in CSS?",
+    answer: "font-weight: bold;",
+    options: [
+      "font-weight: bold;",
+      "text-weight: bold;",
+      "font-bold: true;",
+      "text-bold: true;"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which attribute specifies the image source in an img tag?",
+    answer: "src",
+    options: [
+      "src",
+      "href",
+      "url",
+      "link"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which of the following is a block-level element in HTML?",
+    answer: "div",
+    options: [
+      "a",
+      "img",
+      "div",
+      "span"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Which HTML tag is used to define an internal stylesheet?",
+    answer: "style",
+    options: [
+      "style",
+      "link",
+      "script",
+      "css"
+    ]
+  },
+  {
+    numb: 18,
+    question: "What is the correct syntax for CSS comments?",
+    answer: "/* comment */",
+    options: [
+      "!-- comment --",
+      "/* comment */",
+      "// comment //",
+      "## comment ##"
+    ]
+  },
+  {
+    numb: 19,
+    question: "How do you create a checkbox in HTML?",
+    answer: "input type='checkbox'",
+    options: [
+      "input type='checkbox'",
+      "input type='check'",
+      "checkbox",
+      "input type='box'"
+    ]
+  },
+  {
+    numb: 20,
+    question: "What is the correct HTML tag for inserting an image?",
+    answer: "img",
+    options: [
+      "image",
+      "img",
+      "pic",
+      "src"
+    ]
+  }
+];
